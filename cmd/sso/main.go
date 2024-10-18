24ff0622e043a7f8185daeefc77c246d9763e3bc
sariya <dayzwaitik123@yandex.ru>
2024-10-18 18:58:19 +0300

    add logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
+	"os"
 	"sso/interanal/config"
 )
 
+const (
+	EnvLocal = "local"
+	EnvProd  = "prod"
+)
+
 func main() {
+	ctx := context.Background()
 	config := config.MustLoad()
-	fmt.Println(config)
+	logger := setupLogger(config.Env)
+	logger.LogAttrs(
+		ctx,
+		slog.LevelInfo,
+		"starting application",
+		slog.String("with config", fmt.Sprintf("%+v", config)),
+	)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case EnvLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, nil))
+	case EnvProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	}
+	return log
 }

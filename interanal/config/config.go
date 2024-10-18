cbf99a1119098760ae6b3f11ad8daf4a5a0d1b4c
sariya <dayzwaitik123@yandex.ru>
2024-10-18 17:42:13 +0300

    config read

---
+++ interanal/config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env      string        `yaml:"env" env-required:"true"`
+	TokenTTL time.Duration `yaml:"token_ttl" env-required:"true"`
+	GRPC     GRPCConfig    `yaml:"grpc"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port" env-default:"8080"`
+	Timeout time.Duration `yaml:"timeout" env-default:"1h"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+	if path == "" {
+		panic("config path is empty")
+	}
+	if _, err := os.Stat(path); err != nil {
+		panic("file does not exist: " + path)
+	}
+
+	var config Config
+
+	if err := cleanenv.ReadConfig(path, &config); err != nil {
+		panic("cannot read config: " + err.Error())
+	}
+
+	return &config
+}
+
+// Priority: flag > env > default
+func fetchConfigPath() string {
+	var path string
+	flag.StringVar(&path, "config", "", "path to config file")
+	flag.Parse()
+
+	if path == "" {
+		path = os.Getenv("CONFIG_PATH")
+	}
+	return path
+}
